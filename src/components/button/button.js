fdd19e9a1e4e91bf7797c7f88ef901346e040561
Yagungit <yagun97+001@gmail.com>
2021-06-09 21:19:09 +0300

    button zero arg

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './button.css'
 
 function Button (text) {
+    if ( text === undefined ) {
+        text = 'Hello!';
+    }
     return (
         <button class='button red'>{text}</button>
     );
