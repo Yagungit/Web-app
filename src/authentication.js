0c640581fc64d11b655d7d6e08851be194110431
Yagungit <yagun97+001@gmail.com>
2021-06-11 15:08:47 +0300

    Input and Auth

---
+++ src/authentication.js
@@ -0,0 +1,23 @@
+function useProvideAuth() {
+    const [user, setUser] = useState(null);
+  
+    const signin = cb => {
+      return fakeAuth.signin(() => {
+        setUser("user");
+        cb();
+      });
+    };
+  
+    const signout = cb => {
+      return fakeAuth.signout(() => {
+        setUser(null);
+        cb();
+      });
+    };
+  
+    return {
+      user,
+      signin,
+      signout
+    };
+  }
