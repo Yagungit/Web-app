ebcd3e462e54d13d346dadcdb5d605cee97d7fac
Yagungit <yagun97+001@gmail.com>
2021-06-23 10:14:56 +0300

    major upgate

--- src/containers/homepage.js
+++ src/containers/homepage.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from "react";
+import { useHistory } from 'react-router-dom';
+import  GlobalState  from '../contexts/GlobalState.js';
+
+
+
 
 
 function Home() {
+    const [state, setState] = useContext(GlobalState);
+
+    let history = useHistory();
+
+    useEffect(() => {
+        if (state.auth === false) {
+            history.push('/login');
+        }
+    }, []);
+
     return (
+        
         <div className='home'><h1>Home Screen</h1></div>
     );
   }
