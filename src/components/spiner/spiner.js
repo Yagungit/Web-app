ef0bd86fb21218f8a265147dc8f921290d45c6a7
Yagungit <yagun97+001@gmail.com>
2021-07-26 10:34:24 +0300

    spiner component

---
+++ src/components/spiner/spiner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spiner.css';
+import { FaCog } from 'react-icons/fa'
+
+const Spiner = () => {
+    return(
+        <div>
+            <FaCog className='spiner'/>
+        </div>
+    )
+}
+
+export default Spiner;
